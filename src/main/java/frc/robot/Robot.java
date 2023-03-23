fb684062292374fa03545f0f8df5105db5145d9e
Arnav Thakrar <arnav.thakrar@gmail.com>
2023-03-23 14:18:37 -0400

    driverstation logging too!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -35,7 +36,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Robot Data Logging into CSV files, not just Shuffleboard
     DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+
     ctreConfigs = new CTREConfigs();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
